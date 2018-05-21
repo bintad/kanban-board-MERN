4443c340acbf93d60551eed9eef94de113a0ee25
Joanna Krężołek <j.krezolek@gmail.com>
2018-05-21 18:55:15 +0200

    add drag & drop

---
+++ client/modules/App/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+// Import Style
+import styles from './Footer.css';
+
+export function Footer() {
+  return (
+    <div style={{ background: '#000000' }} className={styles.footer}>
+      <p>&copy; 2016 &middot; Hashnode &middot; LinearBytes Inc.</p>
+      <p><FormattedMessage id="twitterMessage" /> : <a href="https://twitter.com/@mern_io" target="_Blank">@mern_io</a></p>
+    </div>
+  );
+}
+
+export default Footer;
